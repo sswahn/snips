0da37c23ec466e1543a99e9cd6ca13d79affd6c7
sswahn <36422765+sswahn@users.noreply.github.com>
2023-04-17 20:40:39 -0400

    Update dateStuff.js

--- dateStuff.js
+++ dateStuff.js
@@ -6,29 +6,23 @@ const diffBetweenDatesInDays = (date1, date2) =>
 /**
  * Get time Ago
  **/
-// Get the current date and time
-const now = new Date()
-// Set the date to compare against
-const dateToCompare = new Date("2022-03-01T10:00:00Z")
-// Get the difference in milliseconds between the two dates
-const diffInMs = now - dateToCompare;
-// Convert the difference to seconds, minutes, or hours
-let diff, unit;
-if (diffInMs < 60000) { // less than 1 minute
-  diff = Math.floor(diffInMs / 1000)
-  unit = "second"
-} else if (diffInMs < 3600000) { // less than 1 hour
-  diff = Math.floor(diffInMs / 60000)
-  unit = "minute"
-} else { // more than 1 hour
-  diff = Math.floor(diffInMs / 3600000)
-  unit = "hour"
+const timeAgo = date => {
+  const diffInMs = new Date() - date
+  let diff, unit
+  if (diffInMs < 60000) { // less than 1 minute
+    diff = Math.floor(diffInMs / 1000)
+    unit = "second"
+  } 
+  if (diffInMs < 3600000) { // less than 1 hour
+    diff = Math.floor(diffInMs / 60000)
+    unit = "minute"
+  } 
+  if (diffInMs < 86400000) { // more than 1 hour
+    diff = Math.floor(diffInMs / 3600000)
+    unit = "hour"
+  }
+  unit += diff === 1 ? ' ago' : 's ago'
+  return !diff ? ''+date : `${diff} ${unit}`
 }
-// Add the "ago" suffix if necessary
-if (diff === 1) {
-  unit += " ago"
-} else {
-  unit += "s ago"
-}
-// Display the result
-console.log(diff + " " + unit)
+const d = new Date("2023-04-17T10:00:00Z")
+console.log(timeAgo(d))
